e375a868cf96e2f9aec0e20afd86694f7c10a2f9
Kasthoori <ap.kasth@gmail.com>
2024-04-17 20:32:06 +1200

    added new table using model room

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import string
+import random
+
+#To find out a room it need some  ID 
+
+def generate_unique_code():
+    length = 6
+    
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+    return code
 
 # Create your models here.
+
+# Next thing is to create model (table). To do that needs to create 
+# class like this
+
+class Room(models.Model):
+    code = models.CharField(max_length=8, default="", unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=2)
+    created_at = models.DateTimeField(auto_now_add=True)
